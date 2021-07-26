c34237314618e37b64dd3a3eaca1deb6f0101b52
x-idssh <87958653+x-idssh@users.noreply.github.com>
2021-07-26 19:25:14 +0700

    Update about.sh

--- menu/about.sh
+++ menu/about.sh
@@ -5,17 +5,17 @@ echo -e ""
 echo -e "================" | lolcat
 echo -e "|   Powered By  |" | lolcat
 echo -e "----------------" | lolcat
-echo -e "- RedoxxoVpn" | lolcat
+echo -e "- X-IDSSH" | lolcat
 echo -e ""
 echo -e "=================" | lolcat
 echo -e "|   Thanks To   |" | lolcat
 echo -e "-----------------" | lolcat
 echo -e "====================" | lolcat
-echo -e "| Credit By Redoxxovpn |" | lolcat
+echo -e "| Credit By X-IDSSH |" | lolcat
 echo -e "--------------------" | lolcat
-echo -e "Contact   : t.me/anakjati567" | lolcat
+echo -e "Contact   : t.me/sunatmasal99" | lolcat
 echo -e ""
 echo -e "===========================" | lolcat
-echo -e "| ---!  Pakya=Vpn  !--- |" | lolcat
+echo -e "| ---!  X-IDSSH  !--- |" | lolcat
 echo -e "===========================" | lolcat
 echo -e ""
